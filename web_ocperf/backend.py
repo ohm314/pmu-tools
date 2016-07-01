745e168f69dc2b657a05b4ebb4275db1208ba65f
Nikola Hardi <atlantic777@lugons.org>
2016-07-01 09:03:36 +0200

    extracted ocperf_utils module

--- web_ocperf/backend.py
+++ web_ocperf/backend.py
@@ -6,107 +6,15 @@ from flask import Flask, Response, request, send_from_directory, render_template
 import sys
 from bokeh.plotting import figure
 from bokeh.embed import components
+from ocperf_utils import *
 
-STATIC_FILES_ROOT='static'
 RULE_LEN = 80 # just for debug output
-
-# for building workload string (it's list of string args to perf actually)
-CL_PIXEL_PATH = "/home/nhardi/code/cl_forward/bin/x86_64/Release/"
-CL_PIXEL_EXEC = path.join(CL_PIXEL_PATH, "clpixel")
-RESULTS_FILE = path.join(CL_PIXEL_PATH, "test.small.arg")
-CL_PIXEL_CMD = [CL_PIXEL_EXEC, "-serial", "-bin", "-file", RESULTS_FILE]
-
-# utils
-def build_perf_cmd(workload, events_list=None, interval=None):
-    """
-    if event list is empty
-    then run perf stat without -e arguments
-
-    else if event list is NOT empty
-    insert -e args as needed
-
-    if interval is given,
-    add an -I flag
-
-    at last, append workload arguments
-    """
-    cmd = ["perf", "stat", "-x", ","]
-
-    if events_list:
-        cmd += ["-e", ",".join(events_list)]
-
-    if interval:
-        cmd += ["-I", str(interval)]
-
-    cmd += workload
-    return cmd
-
-# TODO use pandas
-def parse_output(raw_output):
-    print(raw_output)
-    # column description
-    # ------------------
-    # 0 - timestamp
-    # 1 - value
-    # 2 - TODO empty
-    # 3 - event type
-    # 4 - TODO (unknown)
-    # 5 - TODO (unknown)
-
-    TIMESTAMP = 0
-    VALUE = 1
-    EVENT_TYPE = 3
-
-    output = {}
-
-    for line in raw_output.split('\n')[:-1]:
-        splitted = line.split(',')
-
-        timestamp = float(splitted[TIMESTAMP])
-        value = int(splitted[VALUE])
-        ev_type = splitted[EVENT_TYPE]
-
-        if ev_type not in output.keys():
-            output[ev_type] = []
-
-        output[ev_type].append( [timestamp, value] )
-
-    return output
-    # return [l.split(',') for l in raw_output.split('\n')[:-1]]
-
-def print_parsed(parsed_output):
-    print("="*RULE_LEN)
-
-    for ev_type in sorted(parsed_output.keys()):
-        print(ev_type)
-        for sample in parsed_output[ev_type]:
-            print(sample[0], sample[1])
-
-    print("="*RULE_LEN)
-
-
-# TODO serializers
-def serialize_results(parsed_output):
-    return json.dumps(parsed_output)
-
-def serialize_emap(emap):
-    d = {}
-
-    for k in emap.events.keys():
-        d[k] = emap.desc[k]
-
-    for k in emap.uncore_events.keys():
-        d[k] = emap.uncored_events[k].desc
-
-    return json.dumps(d, indent=2)
-
-
 
 # main entry point
 def scratch(workload, events, interval):
     # emulating ocperf.py call from cmd line
     print(workload)
-    print(CL_PIXEL_CMD)
+    # print(CL_PIXEL_CMD)
     perf_cmd = build_perf_cmd(workload, events_list=events, interval=interval)
 
     # activating ocperf's mapping of symbolic to msr codes for event arguments
---
+++ web_ocperf/ocperf_utils.py
@@ -0,0 +1,63 @@
+import ocperf as ocp
+from os import path
+import json
+import sys
+
+def build_perf_cmd(workload, events_list=None, interval=None):
+    cmd = ["perf", "stat", "-x", ","]
+
+    if events_list:
+        cmd += ["-e", ",".join(events_list)]
+
+    if interval:
+        cmd += ["-I", str(interval)]
+
+    cmd += workload
+    return cmd
+
+# TODO use pandas
+def parse_output(raw_output):
+    TIMESTAMP = 0
+    VALUE = 1
+    EVENT_TYPE = 3
+
+    output = {}
+
+    for line in raw_output.split('\n')[:-1]:
+        splitted = line.split(',')
+
+        timestamp = float(splitted[TIMESTAMP])
+        value = int(splitted[VALUE])
+        ev_type = splitted[EVENT_TYPE]
+
+        if ev_type not in output.keys():
+            output[ev_type] = []
+
+        output[ev_type].append( [timestamp, value] )
+
+    return output
+
+def print_parsed(parsed_output):
+    print("="*RULE_LEN)
+
+    for ev_type in sorted(parsed_output.keys()):
+        print(ev_type)
+        for sample in parsed_output[ev_type]:
+            print(sample[0], sample[1])
+
+    print("="*RULE_LEN)
+
+
+def serialize_results(parsed_output):
+    return json.dumps(parsed_output)
+
+def serialize_emap(emap):
+    d = {}
+
+    for k in emap.events.keys():
+        d[k] = emap.desc[k]
+
+    for k in emap.uncore_events.keys():
+        d[k] = emap.uncored_events[k].desc
+
+    return json.dumps(d, indent=2)
